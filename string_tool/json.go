e2c4d1100591969d2f8b2f78b8d4076f0ad861fc
yingshaoxo <yingshaoxo@gmail.com>
2022-12-05 11:03:21 +0800

    string_tool: add some functions related to struct object and map and json

--- string_tool/json.go
+++ string_tool/json.go
@@ -1,6 +1,41 @@
 package string_tool
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
+
+func Convert_struct_object_to_map(an_object interface{}) (map[string]interface{}, error) {
+	var a_dict map[string]interface{}
+
+	json_bytes, err := json.Marshal(an_object)
+	if err != nil {
+		return a_dict, err
+	}
+
+	err = json.Unmarshal(json_bytes, &a_dict)
+	if err != nil {
+		return a_dict, err
+	}
+
+	return a_dict, nil
+}
+
+func Convert_map_to_json_string(a_dict map[string]interface{}) string {
+	json_bytes, err := json.Marshal(a_dict)
+	if err != nil {
+		return ""
+	}
+	return Bytes_to_string(json_bytes)
+}
+
+func Convert_struct_object_to_json_string(an_object interface{}) string {
+	json_bytes, err := json.Marshal(an_object)
+	if err != nil {
+		return ""
+	}
+
+	return Bytes_to_string(json_bytes)
+}
 
 func Convert_bytes_json_data_to_map(data []byte) (map[string]interface{}, error) {
 	var result map[string]interface{}
