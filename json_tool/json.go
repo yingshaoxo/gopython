ae8548d150c0f71cc4d2652900a4644d2a4f11ef
yingshaoxo <yingshaoxo@gmail.com>
2023-03-16 17:28:31 +0800

    json_tool: update some input argument type to any

--- json_tool/json.go
+++ json_tool/json.go
@@ -7,7 +7,7 @@ import (
 	"github.com/yingshaoxo/gopython/string_tool"
 )
 
-func Convert_struct_object_to_map(an_object interface{}) (map[string]interface{}, error) {
+func Convert_struct_object_to_map(an_object any) (map[string]interface{}, error) {
 	var a_dict map[string]interface{}
 
 	json_bytes, err := json.Marshal(an_object)
@@ -41,7 +41,7 @@ func Convert_bytes_json_data_to_map(data []byte) (map[string]interface{}, error)
 	return result, nil
 }
 
-func Convert_map_to_json_string(a_dict map[string]interface{}) string {
+func Convert_map_to_json_string(a_dict any) string {
 	json_bytes, err := json.Marshal(a_dict)
 	if err != nil {
 		return ""
@@ -49,7 +49,7 @@ func Convert_map_to_json_string(a_dict map[string]interface{}) string {
 	return string_tool.Bytes_to_string(json_bytes)
 }
 
-func Convert_struct_object_to_json_string(an_object interface{}) string {
+func Convert_struct_object_to_json_string(an_object any) string {
 	json_bytes, err := json.Marshal(an_object)
 	if err != nil {
 		return ""
@@ -58,7 +58,7 @@ func Convert_struct_object_to_json_string(an_object interface{}) string {
 	return string_tool.Bytes_to_string(json_bytes)
 }
 
-func Convert_map_to_struct_object(a_dict map[string]interface{}, the_memory_reference_to_that_object any) {
+func Convert_map_to_struct_object(a_dict any, the_memory_reference_to_that_object any) {
 	var json_string = Convert_map_to_json_string(a_dict)
 	json.Unmarshal([]byte(json_string), the_memory_reference_to_that_object)
 }
