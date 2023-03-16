7f0e7a7892073e7135f242ea07b6ac6e2bc2b386
yingshaoxo <yingshaoxo@gmail.com>
2023-03-16 14:34:00 +0800

    add Get_variable_type_string_representation() and Get_key_value_dict_from_struct_object() to variable_tool

--- tests/variable_tool_test.go
+++ tests/variable_tool_test.go
@@ -30,3 +30,21 @@ func Test_result(t *testing.T) {
 	fmt.Println(a_string.Error)
 	fmt.Println(*a_string.Value.Value)
 }
+
+func Test_type_string_getting(t *testing.T) {
+	hi := "hi"
+
+	var type_string = variable_tool.Get_variable_type_string_representation(hi)
+	fmt.Println(type_string)
+}
+
+type TestClass struct {
+	Name string
+}
+
+func Test_get_dict_from_class_object(t *testing.T) {
+	var test_class = TestClass{Name: "yingshaoxo"}
+
+	fmt.Println(variable_tool.Get_key_value_dict_from_struct_object(test_class, true))
+	fmt.Println(variable_tool.Get_key_value_dict_from_struct_object(test_class, false))
+}
--- variable_tool/variable_tool.go
+++ variable_tool/variable_tool.go
@@ -1,7 +1,10 @@
 // We handle variable related problems here, for example, is the variable nullable
 package variable_tool
 
-import "reflect"
+import (
+	"reflect"
+	"strings"
+)
 
 // T here means a general type
 type Type_Nullable[T any] struct {
@@ -39,6 +42,19 @@ func Is_the_variable_a_struct_object(a_variable any) bool {
 	}
 }
 
+func Is_the_variable_a_list_object(a_variable any) bool {
+	object_type_representation := reflect.TypeOf(a_variable).Kind()
+	if object_type_representation != reflect.Array {
+		return false
+	} else {
+		return true
+	}
+}
+
+func Get_variable_type_string_representation(a_variable any) string {
+	return reflect.TypeOf(a_variable).Name()
+}
+
 func Get_value_from_struct_object_by_name(an_object_instance any, key string) any {
 	object_key_representation := reflect.TypeOf(an_object_instance)
 	object_value_representation := reflect.ValueOf(an_object_instance)
@@ -55,4 +71,24 @@ func Get_value_from_struct_object_by_name(an_object_instance any, key string) an
 	}
 
 	return nil
+}
+
+func Get_key_value_dict_from_struct_object(an_object_instance any, lowercase_key bool) map[string]any {
+	var key_value_dict = make(map[string]any)
+
+	object_key_representation := reflect.TypeOf(an_object_instance)
+	object_value_representation := reflect.ValueOf(an_object_instance)
+
+	for i := 0; i < object_value_representation.NumField(); i++ {
+		var the_key = object_key_representation.Field(i).Name
+		if lowercase_key == true {
+			the_key = strings.ToLower(the_key)
+		}
+		// var the_type = object_type_representation.Field(i).Type.Name()
+		var the_value = object_value_representation.Field(i).Interface()
+
+		key_value_dict[the_key] = the_value
+	}
+
+	return key_value_dict
 }
