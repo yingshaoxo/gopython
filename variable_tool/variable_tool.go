9516c111ebd8f025c04c292d7d5fb6b2160288f5
yingshaoxo <yingshaoxo@gmail.com>
2023-03-16 14:11:44 +0800

    add Is_the_variable_a_struct_object() and Get_value_from_struct_object_by_name() functions to variable_tool

--- variable_tool/variable_tool.go
+++ variable_tool/variable_tool.go
@@ -1,6 +1,8 @@
 // We handle variable related problems here, for example, is the variable nullable
 package variable_tool
 
+import "reflect"
+
 // T here means a general type
 type Type_Nullable[T any] struct {
 	Is_null bool
@@ -26,4 +28,31 @@ func Result[T any](value T) _Result[T] {
 		Value: Nullable[T](item),
 		Error: "",
 	}
+}
+
+func Is_the_variable_a_struct_object(a_variable any) bool {
+	object_type_representation := reflect.TypeOf(a_variable).Kind()
+	if object_type_representation != reflect.Struct {
+		return false
+	} else {
+		return true
+	}
+}
+
+func Get_value_from_struct_object_by_name(an_object_instance any, key string) any {
+	object_key_representation := reflect.TypeOf(an_object_instance)
+	object_value_representation := reflect.ValueOf(an_object_instance)
+
+	for i := 0; i < object_value_representation.NumField(); i++ {
+		var the_key = object_key_representation.Field(i).Name
+		// var the_type = object_type_representation.Field(i).Type.Name()
+		var the_value = object_value_representation.Field(i).Interface()
+
+		if the_key == key {
+			Is_the_variable_a_struct_object(the_value)
+			return the_value
+		}
+	}
+
+	return nil
 }
