a45b99ccb39a9e09a507a8478a78c26ed18a800f
yingshaoxo <yingshaoxo@gmail.com>
2023-03-17 13:09:02 +0800

    update some dict_tool functions, use any when possible

--- dict_tool/dict.go
+++ dict_tool/dict.go
@@ -1,13 +1,13 @@
 // We handle dictionary/map/key-value related problems here
 package dict_tool
 
-func Check_if_a_key_is_in_the_dict(a_dict map[string]any, key string) bool {
-	_, ok := a_dict[key]
+func Check_if_a_key_is_in_the_dict(a_dict any, key string) bool {
+	_, ok := a_dict.(map[string]any)[key]
 	return ok
 }
 
-func Get_dict_value_by_giving_a_key(a_dict map[string]any, key string) any {
-	value, ok := a_dict[key]
+func Get_dict_value_by_giving_a_key(a_dict any, key string) any {
+	value, ok := a_dict.(map[string]any)[key]
 	if ok {
 		return value
 	}
